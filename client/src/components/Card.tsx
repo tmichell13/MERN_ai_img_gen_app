1040a99b438c43b69f2a1ecbbf0e52d80621dee1
tmichell13 <tmichell13@yahoo.com>
2023-03-29 16:30:07 -0400

    finished homepage skeleton, moving to create post page

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Card Component</h1>
+    </div>
+  )
+}
+
+export default Card
---
+++ client/src/components/FormField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormField = () => {
+  return (
+    <div>
+      <h1>Form Field Component</h1>
+    </div>
+  )
+}
+
+export default FormField
